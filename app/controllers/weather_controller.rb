ead044da9edf7bd0093834f56392956ef86bdc31
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2018-05-22 20:34:42 +0900

    add slot

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,8 +2,9 @@ class WeatherController < ApplicationController
   def create
     request = AlexaRubykit::build_request(params)
     response = AlexaRubykit::Response.new
-
-    response.add_speech("こんにちは")
+    # インテントからcityの文字列を受け取る
+    city = request.slots[:city][:value]
+    response.add_speech("#{city}を受け取りました")
     render json: response.build_response
   end
 end
