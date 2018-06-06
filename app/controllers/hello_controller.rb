2101515613c9f094d70568537dc99baaa6a819a4
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2018-06-07 00:15:28 +0900

    [add] こんにちはを返す

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,9 @@
+class HelloController < ApplicationController
+  def create
+    request = AlexaRubykit::build_request(params)
+    response = AlexaRubykit::Response.new
+
+    response.add_speech("こんにちは")
+    render json: response.build_response
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :hello, only: %i(create)
   resources :weather, only: %i(create)
 end
